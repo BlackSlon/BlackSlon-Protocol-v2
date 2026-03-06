ae0b242ad162913b43af5843208b483fe53b42a1
BlackSlon <trading@blackslon.org>
2026-03-06 13:17:40 +0000

    fix: simplify opennext config for dynamic routes

--- out/server/server-reference-manifest.js
+++ out/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"M5UpIbvxxzci/Y6s8Ed6tW/S3X5Hgv8TTmcNCoW17h8=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ZDm1C9c4HBXhUJ0jgxbW1mG12ulJvC7mdeItV+2ykhk=\"\n}"
