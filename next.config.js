a514e5943fb7471240371b83d83612db1f1bc156
BlackSlon <trading@blackslon.org>
2026-03-06 18:39:35 +0000

    cleanup: remove cloudflare config, restore clean next.js for vercel

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Usunęliśmy output: 'export', aby umożliwić dynamiczne trasy dla BSZ
-  // Usunęliśmy distDir: 'out', bo OpenNext używa własnego folderu .open-next
 };
 
 module.exports = nextConfig;
